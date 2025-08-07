2779528a44064ba6e95eb7237e2379867e63a826
seveibar <seveibar@gmail.com>
2025-08-07 11:49:11 -0700

    do some array pulling

--- lib/testing/getInputProblemFromCircuitJsonSchematic.ts
+++ lib/testing/getInputProblemFromCircuitJsonSchematic.ts
@@ -1,9 +1,12 @@
 import type { InputProblem } from "lib/types/InputProblem"
 import type { CircuitJson } from "circuit-json"
+import { cju } from "@tscircuit/circuit-json-util"
 
 export const getInputProblemFromCircuitJsonSchematic = (
   circuitJson: CircuitJson,
 ): InputProblem => {
+  const db = cju(circuitJson)
+
   const problem: InputProblem = {
     chipMap: {},
     chipPinMap: {},
@@ -13,6 +16,24 @@ export const getInputProblemFromCircuitJsonSchematic = (
     pinConnMap: {},
     netConnMap: {},
   }
+
+  const cjChips = db.schematic_component.list().map((schematic_component) => ({
+    schematic_component,
+    source_component: db.source_component.get(
+      schematic_component.source_component_id,
+    ),
+    ports: db.schematic_port
+      .list({
+        schematic_component_id: schematic_component.schematic_component_id,
+      })
+      .map((schematic_port) => ({
+        schematic_port,
+        source_port: db.source_port.get(schematic_port.source_port_id),
+      })),
+  }))
+
+  const cjSourceTraces = db.source_trace.list()
+  const cjSourceNets = db.source_net.list()
 
   // Go through circuit json, pull out source_component/schematic_component as chip
   // ignore groups
