971d1495ed2dadba533730f8df0dd2f9cf18d2ad
SyedHannanMehdi <47053176+SyedHannanMehdi@users.noreply.github.com>
2026-03-30 02:24:08 +0530

    feat: export decoupling capacitor layout from layouts index

---
+++ lib/layouts/index.ts
@@ -0,0 +1,13 @@
+export {
+  layoutDecouplingCapacitors,
+  filterDecouplingPins,
+  isPowerNet,
+} from "./decoupling-capacitor-layout"
+
+export type {
+  Point,
+  PinInfo,
+  ComponentBounds,
+  CapacitorPlacement,
+  DecouplingCapacitorLayoutOptions,
+} from "./decoupling-capacitor-layout"
