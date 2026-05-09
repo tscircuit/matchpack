2c1f8d479418c4dd2778bcd18f81a8c65be3d531
Grant Fletcher <grantfletcher04@gmail.com>
2026-05-09 03:09:52 -0400

    Avoid page dependency in decap solver test

--- tests/IdentifyDecouplingCapsSolver/IdentifyDecouplingCapsSolver06.test.ts
+++ tests/IdentifyDecouplingCapsSolver/IdentifyDecouplingCapsSolver06.test.ts
@@ -1,8 +1,57 @@
 import { test, expect } from "bun:test"
 import { IdentifyDecouplingCapsSolver } from "../../lib/solvers/IdentifyDecouplingCapsSolver/IdentifyDecouplingCapsSolver"
-import { problem } from "../../pages/LayoutPipelineSolver/LayoutPipelineSolver06.page.tsx"
+import type { InputProblem } from "../../lib/types/InputProblem"
 
-test("IdentifyDecouplingCapsSolver identifies decoupling capacitor groups from LayoutPipelineSolver06", () => {
+const problem: InputProblem = {
+  chipMap: {
+    U1: {
+      chipId: "U1",
+      pins: ["U1.1", "U1.2"],
+      size: { x: 2, y: 2 },
+      availableRotations: [0, 90, 180, 270],
+    },
+    C1: {
+      chipId: "C1",
+      pins: ["C1.1", "C1.2"],
+      size: { x: 0.5, y: 1 },
+      availableRotations: [0],
+    },
+    C2: {
+      chipId: "C2",
+      pins: ["C2.1", "C2.2"],
+      size: { x: 0.5, y: 1 },
+      availableRotations: [0],
+    },
+  },
+  chipPinMap: {
+    "U1.1": { pinId: "U1.1", offset: { x: -1, y: 0.5 }, side: "x-" },
+    "U1.2": { pinId: "U1.2", offset: { x: -1, y: -0.5 }, side: "x-" },
+    "C1.1": { pinId: "C1.1", offset: { x: 0, y: 0.5 }, side: "y+" },
+    "C1.2": { pinId: "C1.2", offset: { x: 0, y: -0.5 }, side: "y-" },
+    "C2.1": { pinId: "C2.1", offset: { x: 0, y: 0.5 }, side: "y+" },
+    "C2.2": { pinId: "C2.2", offset: { x: 0, y: -0.5 }, side: "y-" },
+  },
+  netMap: {
+    GND: { netId: "GND", isGround: true },
+    VCC: { netId: "VCC", isPositiveVoltageSource: true },
+  },
+  pinStrongConnMap: {
+    "U1.1-C1.1": true,
+    "C1.1-U1.1": true,
+    "U1.2-C2.1": true,
+    "C2.1-U1.2": true,
+  },
+  netConnMap: {
+    "U1.1-VCC": true,
+    "U1.2-VCC": true,
+    "C1.2-GND": true,
+    "C2.2-GND": true,
+  },
+  chipGap: 0.2,
+  partitionGap: 1,
+}
+
+test("IdentifyDecouplingCapsSolver identifies decoupling capacitor groups", () => {
   const solver = new IdentifyDecouplingCapsSolver(problem)
   solver.solve()
 
