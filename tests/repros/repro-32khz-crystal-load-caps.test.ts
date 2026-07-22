0cf081f1dd29024de5176dd6768c4a65ef8861a8
Mohan <mohn08052006@gmail.com>
2026-07-23 05:22:46 +0530

    repro crystal-capacitor partition (#166)
    * repro crystal circuit partition
    
    * up

---
+++ tests/repros/repro-32khz-crystal-load-caps.test.ts
@@ -0,0 +1,11 @@
+import { expect, test } from "bun:test"
+import { LayoutPipelineSolver } from "lib/solvers/LayoutPipelineSolver/LayoutPipelineSolver"
+import type { InputProblem } from "lib/types/InputProblem"
+import input from "../assets/repro-32khz-crystal-load-caps.input.json"
+
+test("32.768 kHz crystal with two grounded load capacitors", async () => {
+  const solver = new LayoutPipelineSolver(input as InputProblem)
+  solver.solve()
+
+  await expect(solver).toMatchSolverSnapshot(import.meta.path)
+})
