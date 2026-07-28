2d03a4e29d93ce868f90691d16fb6fbaffe7c1bc
mohan-bee <mohn08052006@gmail.com>
2026-07-29 02:41:32 +0530

    mark repro002 loose testpoints

--- tests/repros/repro002.test.ts
+++ tests/repros/repro002.test.ts
@@ -9,5 +9,15 @@ test("repro002", async () => {
   expect(solver.solved).toBe(true)
   expect(solver.failed).toBe(false)
 
+  const testPointIds = ["TP5", "TP6", "TP7", "TP8", "TP9"]
+  const alignTestPointsSolver = solver.alignTestPointsSolver!
+  const outputLayout = solver.getOutputLayout()
+  expect(alignTestPointsSolver.testPointSideGroups).toHaveLength(0)
+  for (const testPointId of testPointIds) {
+    expect(outputLayout.chipPlacements[testPointId]).toEqual(
+      alignTestPointsSolver.inputLayout.chipPlacements[testPointId],
+    )
+  }
+
   await expect(solver).toMatchSolverSnapshot(import.meta.path)
 })
