b1fd33fc92bc29d74199c3502cf552ec210a4170
mohan-bee <mohn08052006@gmail.com>
2026-07-28 22:02:42 +0530

    mark repro002 testpoints

--- tests/repros/repro002.test.ts
+++ tests/repros/repro002.test.ts
@@ -9,5 +9,19 @@ test("repro002", async () => {
   expect(solver.solved).toBe(true)
   expect(solver.failed).toBe(false)
 
+  const testPointIds = ["TP5", "TP6", "TP7", "TP8", "TP9"]
+  const outputLayout = solver.getOutputLayout()
+  expect(solver.alignTestPointsSolver?.unconnectedTestPointAlignment).toEqual({
+    orientation: "horizontal",
+    chipIds: testPointIds,
+    perpendicularOffset: 0,
+  })
+  expect(
+    new Set(
+      testPointIds.map((chipId) => outputLayout.chipPlacements[chipId]!.y),
+    ).size,
+  ).toBe(1)
+  expect(solver.checkForOverlaps(outputLayout)).toHaveLength(0)
+
   await expect(solver).toMatchSolverSnapshot(import.meta.path)
 })
